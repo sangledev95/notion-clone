3928f271f95768fe6e01442f710d3951bc13f9de
sangledev95 <lqsang19952@gmail.com>
2025-03-14 16:21:36 +0700

    Add iis deploy

--- server.js
+++ server.js
@@ -2,37 +2,27 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
-
-const port = process.env.PORT || 5000; // Change the port to the port that your IIS is running on. Default its 80 and 3000 if you are using it for developing.
-const hostname = "localhost";
-const app = next({ dev, hostname, port });
+const port = process.env.PORT || 3004; // Đảm bảo trùng với port IIS
+const hostname = "0.0.0.0"; // Lắng nghe tất cả địa chỉ IP
+const app = next({ dev: false, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
       const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
-
-      if (pathname === "/a") {
-        await app.render(req, res, "/a", query);
-      } else if (pathname === "/b") {
-        await app.render(req, res, "/b", query);
-      } else {
-        await handle(req, res, parsedUrl);
-      }
+      await handle(req, res, parsedUrl);
     } catch (err) {
       console.error("Error occurred handling", req.url, err);
       res.statusCode = 500;
-      res.end("internal server error");
+      res.end("Internal Server Error");
     }
   })
     .once("error", (err) => {
-      console.error(err);
+      console.error("Server error:", err);
       process.exit(1);
     })
-    .listen(port, async () => {
+    .listen(port, () => {
       console.log(`> Ready on http://localhost:${port}`);
     });
 });
